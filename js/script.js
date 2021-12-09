23377f898c96bd245e7b65ff00f1801de07abafb
SimoneSpirito <simospiri01@gmail.com>
2021-12-09 13:34:13 +0100

    email list

--- js/script.js
+++ js/script.js
@@ -2,12 +2,23 @@
 const app = new Vue({
     el: '#root',
     data: {
-
+        mails: []
     },
     methods: {
 
     },
     created(){
-        
+        for (let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // handle success
+                this.mails.push(response.data.response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+        }
+        console.log(this.mails)
     }
 })
